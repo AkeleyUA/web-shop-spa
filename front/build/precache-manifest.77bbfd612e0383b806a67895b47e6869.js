a912ac454c4701939e94c75038693e7079971df3
Alexandr <Akeley1994@gmail.com>
2020-05-05 05:17:19 +0300

    first deploy

---
+++ front/build/precache-manifest.77bbfd612e0383b806a67895b47e6869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c29797ba7edf14a3640525f23660352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad935ff40c5238363a4c",
+    "url": "/static/css/main.4b10c626.chunk.css"
+  },
+  {
+    "revision": "eabea1f75a0809efbdeb",
+    "url": "/static/js/2.697a8d9e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.697a8d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad935ff40c5238363a4c",
+    "url": "/static/js/main.b317f9bb.chunk.js"
+  },
+  {
+    "revision": "24b486c66c3a7276b55f",
+    "url": "/static/js/runtime-main.246e8137.js"
+  }
+]);
