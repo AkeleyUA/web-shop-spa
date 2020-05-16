9ac91abaa4f655d5d9426ec317029896a6240c7a
Alexandr <Akeley1994@gmail.com>
2020-05-16 11:33:04 +0300

    build for heroku

---
+++ front/build/precache-manifest.832a14dbd6144a2eb2be8ff6c8cc2958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f27e2eec68d453233ff1fdd22cf058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "558c84e12ed30c1bc113",
+    "url": "/static/css/main.f0a47a7c.chunk.css"
+  },
+  {
+    "revision": "34a328efdad08e80aa2a",
+    "url": "/static/js/2.1544d4c3.chunk.js"
+  },
+  {
+    "revision": "884d6422099fcad6c31f436a36163b39",
+    "url": "/static/js/2.1544d4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558c84e12ed30c1bc113",
+    "url": "/static/js/main.686a4070.chunk.js"
+  },
+  {
+    "revision": "24b486c66c3a7276b55f",
+    "url": "/static/js/runtime-main.246e8137.js"
+  }
+]);
